52821c4529b8e431f0ea1d1ff242ea8a6c7ece84
Sarah Handal <sarah.handal@gmail.com>
2025-07-22 19:48:04 -0600

    updating the frontend

--- frontend/src/components/Header/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import logo from "../../../assets/images/logo.png";
-
-const Logo = () => {
-  return (
-  <img src={logo} alt="logo-page"/>
-  );
-}
-
-export default Logo;
--- frontend/src/utils/messages.js
+++
@@ -1,8 +0,0 @@
-const messages = {
-    registerTrue: '¡Correcto! Ya estás registrado.',
-    registerFalse: 'Uy, algo salió mal. Por favor, inténtalo de nuevo.',
-    linkaltFalse: 'Error',
-    linkaltTrue: 'Correcto',
-  };
-  
-  export default messages;
